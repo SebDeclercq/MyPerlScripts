7990f152da076503d558a79b4e19a7da3d3d3895
SebDeclercq <miniSeb3.0@gmail.com>
2012-03-01 19:31:48 +0100

    Adding IP checking script (+ email alerts)

---
+++ ipcheck.pl
@@ -0,0 +1,29 @@
+#! /usr/bin/perl -w
+use strict;
+use warnings;
+use LWP::Simple;
+use File::Slurp;
+# REQUIRES sendemail to work
+# ( sudo apt-get install sendemail )
+
+my ($ipFile,$oldIP) = ("dyndnscheck",0);
+chomp($oldIP = read_file($ipFile)) if -e $ipFile;
+
+
+my $link = get("http://checkip.dyndns.org")
+  or die "can't fetch url : $!\n";
+my $currIP = $& if $link =~ /\d+(\.\d+){3}/;
+
+
+if ($oldIP eq $currIP) {
+  next;
+} else {
+  my ($email,$password,$smtp) =   # Defining personal information here ;
+  my ($title,$message) = ('IP has changed !',"New IP is : $currIP\n");
+
+  !system 'sendemail','-m', $message,'-f', $email,'-t', $email ,
+    '-u', $title ,'-s', $smtp ,'-o', 'tls=yes','-xu', $email, '-xp', $password
+      or die "can't use sendemail...";
+
+  write_file($ipFile,$currIP);
+}
