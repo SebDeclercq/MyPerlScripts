63bcac5a246da9640c12498b912c23e4aedf8584
sebdeclercq <miniseb3.0@gmail.com>
2015-02-12 17:17:54 +0100

    1st ruby script

--- prog_ruby_dl.rb
+++ prog_ruby_dl.rb
@@ -1,47 +1,39 @@
 #!/usr/bin/ruby1.9.3
 
-# = LWP::UserAgent
-require 'mechanize'
+require 'mechanize' # = LWP::UserAgent
 
-# Output directory
 dir = 'Programming_Ruby'
-# Base URL
 base_url = "http://ruby-doc.com/docs/ProgrammingRuby/"
 
-# Make dir and change into it
 Dir.mkdir(dir) unless File.exists? dir
 Dir.chdir(dir) or abort "Unable to change to \"#{dir}\" : #{$!}"
 
-# initialize mechanize
-agent = Mechanize.new
-# get page content
-page = agent.get(base_url + "pr_toc.html")
+agent = Mechanize.new                      # initialize mechanize
+page = agent.get(base_url + "pr_toc.html") # get page content
 
-# open file 'index.html', write page content and close file
-toc_file = File.new( 'index.html', 'w' )
+toc_file = File.new( 'index.html', 'w' )   # Create toc file (for calibre)
 
-header = %{<html><body><h1>Table of Contents</h1><p style="text-indent:0pt">}
-toc_file.puts(header)
+header = %{                                # Header for index.html
+<html>
+  <body>
+    <h1>Table of Contents</h1>
+    <p style="text-indent:0pt">
+}
+toc_file.puts(header)                      # Print header to index.html
 
 i = 0
-# for each link
-page.search('a').each do |a|
+page.search('a').each do |a|                               # For each link
   i += 1
-  # replace all spaces by underscores
-  title = a.text.tr(' ','_')
-  # filename = 00_title.html
-  filename = i.to_s.rjust(2,'0') + '_' + title + '.html'
-  # open/create file
-  output = File.new( filename, 'w' )
-  # get content of the linked page
-  content = agent.get base_url + a["href"]
-  # print it in output file
-  output.puts(content.body)
-  # close output file
-  output.close
+  title = a.text.tr(' ','_')                               # replace all spaces by underscores
+  filename = i.to_s.rjust(2,'0') + '_' + title + '.html'   # filename = 00_title.html
+  output = File.new( filename, 'w' )                       # open/create file
+  content = agent.get base_url + a["href"]                 # get content of the linked page
+  output.puts(content.body)                                # print it in output file
+  output.close                                             # close output file
 
+  # Add link to file in index.html
   toc_line = "<a href='" + filename + "'>" + a.text + "</a>"
   toc_file.puts(toc_line)
 end
 
-footer = %{</p></body></html>}
+footer = %{</p></body></html>}             # Print footer to index.html
